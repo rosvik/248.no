2a98527ab9318082f4cc91bb03fd70d095cb5934
rosvik <j.rosvik@gmail.com>
2018-02-08 00:53:15 +0100

    Scan algorithm improvements

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ https://248.no
 
 ## Credits
 
-### setTimeout with a shorter delay ([dbaron.org](https://dbaron.org/log/20100309-faster-timeouts))
+### setTimeout with a shorter delay
 Most browsers don't support a delay shorter than 10 milliseconds, so by using some magic hack with `window.postMessage()`, you can get a shorter delay that can run around 50 times faster. On my page it makes the cursor run about twice as fast as `setTimeout(func, 0)` would.
 
 https://dbaron.org/log/20100309-faster-timeouts
--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
 var text_height = 13;
 var blank_char = '0';
 var text_class = ".fill_text";
-var startTime = Date.now();
+var cursor_color = 'red';
 
 /*
 	Inititalize variables
@@ -17,80 +17,68 @@ var init_text = "";
 var hello = ":::    ::: :::::::::: :::        :::        ::::::::  ::: \n:+:    :+: :+:        :+:        :+:       :+:    :+: :+: \n+:+    +:+ +:+        +:+        +:+       +:+    +:+ +:+ \n+#++:++#++ +#++:++#   +#+        +#+       +#+    +:+ +#+ \n+#+    +#+ +#+        +#+        +#+       +#+    +#+ +#+ \n#+#    #+# #+#        #+#        #+#       #+#    #+#     \n###    ### ########## ########## ########## ########  ### ";
 var art_line_lenght = hello.indexOf("\n")+1;
 var art_line_count = (hello.match(/\n/g) || []).length+1;
-
+var text_element = null;
 $(document).ready(function() {
 
-
-	/*
-		Inititalize document
-	*/
-
-	var text_element = $(text_class);
+	text_element = $(text_class);
+	text_element.html('|'.repeat(512));
+	text_element.lettering();
 	line_length = get_line_length(text_element);
+	text_element.html('');
 	init_text = blank_char.repeat(line_length).repeat(text_height);
 	char_count = init_text.length;
 	text_element.html(init_text).lettering();
 
+	// Inititalize document
+	init_document();
+
+	// Start cursor loop
 	myLoop(0, char_count, function(i) {
-		// Clear cursor fill
-		var char_element = $('.char' + i);
-		char_element.css('background-color', 'transparent');
-
-		// Find coordinates
-		var y = parseInt(i / line_length);
-		var x = i % line_length;
-		if (x == 0) x = line_length;
-
-		// Insert art character at [x,y]
-		var offset = parseInt(0.5*line_length - 0.5*art_line_lenght);
-		insertArtAt(x, y, offset, 3, char_element);
-
-		// Set cursor fill
-		var next_char = i+1;
-		$('.char' + next_char ).css('background-color', '#555');
+		setLetter(i);
 	});
 });
 
+
+
 function myLoop (s, f, action) {
-	if (++s < f) {
+	if (++s <= f) {
 		action(s);
 		setZeroTimeout(function(){
 			myLoop(s,f,action)
 		});
 	} else {
 		// After loop finishes
-		myLoop(0, char_count, action)
+		init_document()
+		myLoop(0, char_count, function(i) {
+			setLetter(i);
+		});
 	}
 };
 
-function insertArtAt(x, y, dx, dy, element) {
-	if (x < art_line_lenght+dx && x > dx &&  // Check if [x,y] in range from
-		y < art_line_count+dy && y >= dy) {  // [x+dx, y+dy] to [all+dx, alc+dy]
-		var art_char = art_line_lenght * (y-dy) + (x-dx);
-		if (hello[art_char-1] != " ") {
-			element.html(hello[art_char-1]);
-			element.css('color', 'white');
-		}
-	}
-}
 
-function countLetterByLine(element) {
-	var lineLetterCount = {};
-	for (var i = 0; i < element.children().length; i++) {
-		var y = element.children().eq(i).position().top;
-		if (lineLetterCount[y] == undefined) {
-			lineLetterCount[y] = 1;
-		} else {
-			lineLetterCount[y] = lineLetterCount[y] + 1;
+/*
+	Inititalize document
+*/
+function init_document() {
+	// Calculate variables
+
+	line_length = get_line_length(text_element);
+
+	function countLetterByLine(element) {
+		var lineLetterCount = {};
+		for (var i = 0; i < element.children().length; i++) {
+			var y = element.children().eq(i).position().top;
+			if (lineLetterCount[y] == undefined) {
+				lineLetterCount[y] = 1;
+			} else {
+				lineLetterCount[y] = lineLetterCount[y] + 1;
+			}
 		}
+		return lineLetterCount;
 	}
-	return lineLetterCount;
 }
 
 function get_line_length(element) {
-	element.html('|'.repeat(512));
-	element.lettering();
-
 	var top = element.children().eq(0).position().top;
 	var n = 0;
 	for (var i = 0; i < element.children().length; i++) {
@@ -100,6 +88,50 @@ function get_line_length(element) {
 			break;
 		}
 	}
-	element.html('');
 	return n;
+}
+
+
+/*
+	Do character calculations and change letter
+*/
+function setLetter(i) {
+	// Clear cursor fill
+	var char_element = $('.char' + i);
+	char_element.css('background-color', 'transparent');
+
+	// Find coordinates
+	var y = parseInt(i / line_length);
+	var x = i % line_length;
+	if (x == 0) x = line_length;
+
+	// Insert art character in center of canvas
+	var offset = parseInt(0.5*line_length - 0.5*art_line_lenght) + 1;
+	insertArtAt(x, y, offset, 3, char_element);
+
+	// Set cursor fill
+	var next_char = i+1;
+	$('.char' + next_char ).css('background-color', cursor_color);
+
+	/*
+		Insert art character at [x,y]
+	*/
+	function insertArtAt(x, y, dx, dy, element) {
+		if (x < art_line_lenght+dx && x > dx &&  // Check if [x,y] in range from
+			y < art_line_count+dy && y >= dy) {  // [x+dx, y+dy] to [all+dx, alc+dy]
+			var art_char = art_line_lenght * (y-dy) + (x-dx);
+			if (hello[art_char-1] != " ") {
+				element.html(hello[art_char-1]);
+				element.css('color', 'white');
+			} else {
+				char_reset();
+			}
+		} else {
+			char_reset();
+		}
+		function char_reset() {
+			element.html('0');
+			element.css('color', 'inherit');
+		}
+	}
 }
