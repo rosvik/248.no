af8465915104e4977feb968b87a3da02e453eb67
rosvik <j.rosvik@gmail.com>
2018-02-08 01:07:33 +0100

    Changing text demo

--- js/main.js
+++ js/main.js
@@ -15,11 +15,16 @@ var line_length = 0
 var init_text = "";
 // Generated at http://patorjk.com/software/taag/#p=testall&f=Contrast&t=hello!
 var hello = ":::    ::: :::::::::: :::        :::        ::::::::  ::: \n:+:    :+: :+:        :+:        :+:       :+:    :+: :+: \n+:+    +:+ +:+        +:+        +:+       +:+    +:+ +:+ \n+#++:++#++ +#++:++#   +#+        +#+       +#+    +:+ +#+ \n+#+    +#+ +#+        +#+        +#+       +#+    +#+ +#+ \n#+#    #+# #+#        #+#        #+#       #+#    #+#     \n###    ### ########## ########## ########## ########  ### ";
+
+var hello2 = "::::::::::: ::: ::::    ::::       ::::::::::: ::::::::  :::    ::: ::::    ::: \n    :+:     :+  +:+:+: :+:+:+          :+:    :+:    :+: :+:    :+: :+:+:   :+: \n    +:+         +:+ +:+:+ +:+          +:+    +:+    +:+ +:+    +:+ :+:+:+  +:+ \n    +#+         +#+  +:+  +#+          +#+    +#+    +:+ +#++:++#++ +#+ +:+ +#+ \n    +#+         +#+       +#+          +#+    +#+    +#+ +#+    +#+ +#+  +#+#+# \n    #+#         #+#       #+#      #+# #+#    #+#    #+# #+#    #+# #+#   #+#+# \n###########     ###       ###       #####      ########  ###    ### ###    #### "
+var hello3 = "::::::::: :::    ::: :::::::::   :::::::::  \n     :+:  :+:    :+: :+:    :+: :+:     :+: \n    +:+   +:+    +:+ +:+    +:+        +:+  \n   +#+    +#+    +:+ +#++:++#+        +#+   \n  +#+     +#+    +#+ +#+            +#+     \n #+#      #+#    #+# #+#                    \n#########  ########  ###            ###     "
 var art_line_lenght = hello.indexOf("\n")+1;
 var art_line_count = (hello.match(/\n/g) || []).length+1;
 var text_element = null;
+
 $(document).ready(function() {
 
+	// Inititalize document (only first time)
 	text_element = $(text_class);
 	text_element.html('|'.repeat(512));
 	text_element.lettering();
@@ -29,7 +34,7 @@ $(document).ready(function() {
 	char_count = init_text.length;
 	text_element.html(init_text).lettering();
 
-	// Inititalize document
+	// Inititalize document (on every loop)
 	init_document();
 
 	// Start cursor loop
@@ -39,6 +44,9 @@ $(document).ready(function() {
 });
 
 
+/*
+	Cursor loop
+*/
 
 function myLoop (s, f, action) {
 	if (++s <= f) {
@@ -49,6 +57,8 @@ function myLoop (s, f, action) {
 	} else {
 		// After loop finishes
 		init_document()
+		hello = hello3;
+		art_line_lenght = hello.indexOf("\n")+1;
 		myLoop(0, char_count, function(i) {
 			setLetter(i);
 		});
@@ -57,25 +67,12 @@ function myLoop (s, f, action) {
 
 
 /*
-	Inititalize document
+	Inititalize document (on every loop)
 */
 function init_document() {
 	// Calculate variables
 
 	line_length = get_line_length(text_element);
-
-	function countLetterByLine(element) {
-		var lineLetterCount = {};
-		for (var i = 0; i < element.children().length; i++) {
-			var y = element.children().eq(i).position().top;
-			if (lineLetterCount[y] == undefined) {
-				lineLetterCount[y] = 1;
-			} else {
-				lineLetterCount[y] = lineLetterCount[y] + 1;
-			}
-		}
-		return lineLetterCount;
-	}
 }
 
 function get_line_length(element) {
@@ -119,6 +116,8 @@ function setLetter(i) {
 	function insertArtAt(x, y, dx, dy, element) {
 		if (x < art_line_lenght+dx && x > dx &&  // Check if [x,y] in range from
 			y < art_line_count+dy && y >= dy) {  // [x+dx, y+dy] to [all+dx, alc+dy]
+
+			// Set character from art
 			var art_char = art_line_lenght * (y-dy) + (x-dx);
 			if (hello[art_char-1] != " ") {
 				element.html(hello[art_char-1]);
