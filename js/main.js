716704f4e793f38cd63896772345737564cf47b0
rosvik <j.rosvik@gmail.com>
2018-02-11 17:05:45 +0100

    main.js improvements

--- js/main.js
+++ js/main.js
@@ -11,9 +11,15 @@ var dy = 3;
 /*
 	Inititalize variables
 */
-var char_count = 0;
-var line_length = 0
-var init_text = "";
+var char_count = 0,
+	line_length = 0,
+	init_text = "",
+	art_line_lenght = 0,
+	art_line_count = 0,
+	dx = 0,
+	text_element = null,
+	prev_char_element = null;
+
 // Generated at http://patorjk.com/software/taag/
 var hello = ":::    ::: :::::::::: :::        :::        ::::::::  ::: \n:+:    :+: :+:        :+:        :+:       :+:    :+: :+: \n+:+    +:+ +:+        +:+        +:+       +:+    +:+ +:+ \n+#++:++#++ +#++:++#   +#+        +#+       +#+    +:+ +#+ \n+#+    +#+ +#+        +#+        +#+       +#+    +#+ +#+ \n#+#    #+# #+#        #+#        #+#       #+#    #+#     \n###    ### ########## ########## ########## ########  ### ";
 
@@ -23,22 +29,16 @@ var hello3 = "::::::::: :::    ::: :::::::::   :::::::::  \n     :+:  :+:    :+:
 
 var hello4 = " ::::::::   :::      ::::::::      ::::    :::  ::::::::  \n:+:    :+: :+:      :+:    :+:     :+:+:   :+: :+:    :+: \n      +:+ +:+ +:+   +:+    +:+     :+:+:+  +:+ +:+    +:+ \n    +#+  +#+  +:+    +#++:++#      +#+ +:+ +#+ +#+    +:+ \n  +#+   +#+#+#+#+#+ +#+    +#+     +#+  +#+#+# +#+    +#+ \n #+#          #+#   #+#    #+# #+# #+#   #+#+# #+#    #+# \n##########    ###    ########  ### ###    ####  ########  ";
 
-var art_line_lenght = hello.indexOf("\n")+1;
-var art_line_count = (hello.match(/\n/g) || []).length+1;
-var text_element = null;
-var dx = parseInt(0.5*line_length - 0.5*art_line_lenght) + 1;
-
 $(document).ready(function() {
 	// Inititalize document (only first time)
 	text_element = $(text_class);
-	text_element.html('|'.repeat(512));
+	text_element.html(blank_char.repeat(512));
 	text_element.lettering();
 	line_length = get_line_length(text_element);
 	text_element.html('');
 	init_text = blank_char.repeat(line_length).repeat(text_height);
 	char_count = init_text.length;
 	text_element.html(init_text).lettering();
-	dx = parseInt(0.5*line_length - 0.5*art_line_lenght) + 1;
 
 	// Inititalize document (on every loop)
 	init_document();
@@ -62,8 +62,12 @@ function myLoop (s, f, action) {
 	var x = s % line_length;
 	if (x == 0) x = line_length;
 
+	// Run action() on every character
+	//if(s<=f) {
+
+	// Run action() if needed
 	if (s <= f && (need_replace(s) || need_insert(s, x, y))) {
-	//if (s <= f) {
+
 		action(s, x, y);
 		// console.log("woo");
 		setZeroTimeout(function(){
@@ -73,18 +77,17 @@ function myLoop (s, f, action) {
 		myLoop(s,f,action);
 	} else if (s > f) {
 		// After loop finishes
-		init_document()
 		hello = hello4;
-		art_line_lenght = hello.indexOf("\n")+1;
-		// myLoop(0, char_count, function(i) {
-		// 	setLetter(i);
-		// });
+		init_document();
+		myLoop(0, char_count, function(i, x, y) {
+			setLetter(i, x, y);
+		});
 	}
 
 	function need_replace(i) {
 		var char_element = $('.char' + i);
 		// console.log(char_element.html() != 0);
-		return char_element.html() != 0;
+		return char_element.html() != blank_char;
 	}
 
 	function need_insert(i, x, y) {
@@ -108,8 +111,10 @@ function myLoop (s, f, action) {
 */
 function init_document() {
 	// Calculate variables
-
 	line_length = get_line_length(text_element);
+	art_line_lenght = hello.indexOf("\n")+1;
+	art_line_count = (hello.match(/\n/g) || []).length+1;
+	dx = parseInt(0.5*line_length - 0.5*art_line_lenght) + 1;
 }
 
 /*
@@ -134,16 +139,15 @@ function get_line_length(element) {
 */
 function setLetter(i, x, y) {
 	// Clear cursor fill
+	if (prev_char_element != null) prev_char_element.css('background-color', 'transparent');
 	var char_element = $('.char' + i);
-	char_element.css('background-color', 'transparent');
+	char_element.css('background-color', cursor_color);
 
 	// Insert art character in center of canvas
 	// dx = parseInt(0.5*line_length - 0.5*art_line_lenght) + 1;
 	insertArtAt(x, y, dx, dy, char_element);
 
-	// Set cursor fill
-	var next_char = i+1;
-	$('.char' + next_char ).css('background-color', cursor_color);
+	prev_char_element = char_element;
 
 	/*
 		Insert art character at [x,y]
@@ -164,7 +168,7 @@ function setLetter(i, x, y) {
 			char_reset();
 		}
 		function char_reset() {
-			element.html('0');
+			element.html(blank_char);
 			element.css('color', 'inherit');
 		}
 	}
