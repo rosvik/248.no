9917f4302092c63684761b427c0c9ba5bbb831b7
Johannes Røsvik <j.rosvik@gmail.com>
2024-01-24 00:20:04 +0100

    catch errors when getting COMMIT_HASH

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -5,8 +5,14 @@ export const ssr = true;
 export const csr = false;
 
 export const load = ({ request }) => {
+  let hash;
+  try {
+    hash = process.env.COMMIT_HASH || process.env.CF_PAGES_COMMIT_SHA || undefined;
+  } catch (e) {
+    // console.warn(e);
+  }
   return {
-    hash: process.env.COMMIT_HASH || undefined,
+    hash,
     date: new Date().toUTCString(),
     year: new Date().getFullYear(),
     ip: getIP(request),
