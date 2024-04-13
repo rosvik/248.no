604745a50030bf8c1e3d668836a237a1aeccbf0f
Johannes Røsvik <j.rosvik@gmail.com>
2024-04-14 00:57:02 +0200

    remove cloudflare provider

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://248.no',
+  base: "/",
   output: "server",
-  adapter: cloudflare(),
+  trailingSlash: "ignore",
 });
