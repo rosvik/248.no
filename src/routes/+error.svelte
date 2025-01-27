252bcaad0faa65ef83418ac3950b364dc9ccd99c
Johannes Røsvik <j.rosvik@gmail.com>
2025-01-28 00:08:53 +0100

    update 404 page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,9 +2,6 @@
 	import { page } from '$app/state';
 </script>
 <pre>
-<span>{page.status} {page.error?.message}</span>
-
-
           ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠀⡀⢀⠀⢠⠀⠀⠀⠀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
           ⠀⠀⠀⠀⠀⠀⢠⢤⣀⠀⠀⠀⠈⣆⢧⠈⡆⢸⠀⠀⠀⢰⢡⠇⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀
           ⠀⠀⠀⢀⠀⠀⣯⢀⣨⠃⠀⠀⠀⠸⡜⣄⣣⢸⠀⠀⠀⡜⡌⠀⠀⠀⠀⢀⡜⡁⠀⠀⠀⠀⠀
@@ -25,6 +22,9 @@
           ⠀⠀⠀⠀⠀⠀⠀⢀⠞⡼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⡀⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀
           ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 
+
+<span>{page.status} {page.error?.message}</span>
+
 Oupsie doopsie! There seems to have been an explosion at our
 banana factory, and our code monkeys are on strike. We've
 asked the Twitter Fail Whale to step in and help us out, but
@@ -37,8 +37,10 @@ need to treat every HTTP request as a new opportunity to
 learn and grow.
 
 Of course, it is quite possible that you mistyped the URL,
-which is an honest mistake. Either way, we're sorry for the
-inconvenience, and hope you have a great rest of your day.
+which is an common mistake anyone can make. So if that's the
+case, there's no need to feel bad about it. Either way,
+we're sorry for the inconvenience, and hope you have a great
+rest of your day :)
 
-<span><a href="/">Click here to get out of here</a></span>
+<a href="/">Click here to get out of here</a>
 </pre>
