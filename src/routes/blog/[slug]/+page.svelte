13fdc1ed4f9697a9449cebd8e504ea5592ed665c
Johannes Røsvik <j.rosvik@gmail.com>
2026-06-05 15:09:00 +0200

    feat: add open graph data to blog posts

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -3,6 +3,7 @@
   import { marked } from 'marked';
   import { formatId, formatDate } from '$lib/utils';
   import { error } from '@sveltejs/kit';
+  import { page } from '$app/state';
   import type { PageData } from './$types';
 
   export let data: PageData;
@@ -15,6 +16,15 @@
 
 <svelte:head>
   <title>{data.title} - 248.no</title>
+
+  <meta property="og:type" content="article" />
+  <meta property="og:title" content={data.title} />
+  <meta property="twitter:title" content={data.title} />
+  <meta property="og:url" content={page.url.href} />
+  <meta property="og:site_name" content="248.no" />
+  <meta property="article:published_time" content={new Date(data.published).toISOString()} />
+  {#if data.updated}<meta property="article:modified_time" content={new Date(data.updated).toISOString()} />{/if}
+  <meta property="article:author" content={data.author.name} />
 </svelte:head>
 
 <pre>{formatId(data.id)} • {data.slugname}</pre>
